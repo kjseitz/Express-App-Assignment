625aa04891163525913281ef20d9f95f4f422c76
kjseitz <kjseitz@gus.pittstate.edu>
2023-11-12 14:55:16 -0600

    added the dashboard route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET dashboard page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard' });
-});
-
-module.exports = router;
+    res.render('dashboard');
+  });
+  
