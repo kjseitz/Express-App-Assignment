2d58b678de959a11abdcfb87d506ce33e063efe9
kjseitz <kjseitz@gus.pittstate.edu>
2023-11-12 12:06:00 -0600

    set up index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // run `node index.js` in the terminal
+var express = require('express');
+var app = express();
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+});
