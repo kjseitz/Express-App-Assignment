7f5d5bfe3e9672c37b0c8aac8324d6792d702f2b
kjseitz <kjseitz@gus.pittstate.edu>
2023-11-13 09:55:20 -0600

    fixed profile data

--- routes/profile.js
+++ routes/profile.js
@@ -20,9 +20,6 @@ router.get('/', function(req, res, next) {
           },
           phone: "1-770-736-8031 x56442",
           website: "hildegard.org",
-          company: {
-            name: "Romaguera-Crona"
-          }
         },
     ]});
   });
