5df5506a8726f6c3c8a4aa2019d0c7fd3f5b7937
kjseitz <kjseitz@gus.pittstate.edu>
2023-11-12 20:29:14 -0600

    deleted placeholder data

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,14 +3,6 @@ var router = express.Router();
 
 /* GET dashboard page. */
 router.get('/', function(req, res, next) {
-    res.render('dashboard'),
-    userData = [
-    {
-      "userId": 1,
-      "id": 8,
-      "title": "dolorem dolore est ipsam",
-      "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
-    }];
-  });
+    res.render('dashboard')});
   
   module.exports = router;
