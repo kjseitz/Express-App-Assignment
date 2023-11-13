a068b2b7ca860bfdd58f827e57f89c33a2b19d73
kjseitz <kjseitz@gus.pittstate.edu>
2023-11-13 09:49:12 -0600

    finished profile data

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET profile page. */
 router.get('/', function(req, res, next) {
-    res.render('profile');
+    res.render('profile',
+    {
+      profileData:
+      [
+        {
+          id: 1,
+          name: "Leanne Graham",
+          username: "Bret",
+          email: "Sincere@april.biz",
+          address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+          },
+          phone: "1-770-736-8031 x56442",
+          website: "hildegard.org",
+          company: {
+            name: "Romaguera-Crona"
+          }
+        },
+    ]});
   });
   
   module.exports = router;
