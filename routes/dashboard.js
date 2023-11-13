7b53d4b75d41676fe593c3900f1bbb42891db4f3
kjseitz <kjseitz@gus.pittstate.edu>
2023-11-12 20:16:01 -0600

    added placeholder data

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET dashboard page. */
 router.get('/', function(req, res, next) {
-    res.render('dashboard');
+    res.render('dashboard'),
+    userData = [
+    {
+      "userId": 1,
+      "id": 8,
+      "title": "dolorem dolore est ipsam",
+      "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+    }];
   });
   
   module.exports = router;
